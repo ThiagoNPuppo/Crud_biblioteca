1460f1bca03d884db671ec2023b133b593c88b6f
Thiago Nunes Puppo <127260159+ThiagoNPuppo@users.noreply.github.com>
2023-11-05 12:16:26 -0300

    Sem Router

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const userRouter = require('./router/userRouter');
-const bookRouter = require('./router/bookRouter');
-//const userController = require('./controller/userController');
-//const bookController = require('./controller/bookController');
+//const userRouter = require('./router/userRouter');
+//const bookRouter = require('./router/bookRouter');
+const userController = require('./controller/userController');
+const bookController = require('./controller/bookController');
 
 const app = express();
 const port = 3000;
@@ -15,25 +15,25 @@ app.get('/', (req, res) => {
 
 
 
-app.use('/users', userRouter);
-app.use('/books', bookRouter);
+//app.use('/users', userRouter);
+//app.use('/books', bookRouter);
 
 
-// app.get('/users', userController.listarUsuarios);
-// app.get('/users/:id', userController.buscarUsuario);
-// app.post('/users', userController.adicionarUsuario);
-// app.delete('/users/:id', userController.removerUsuario);
-// app.put('/users/:id', userController.atualizarUsuario);
+app.get('/users', userController.listarUsuarios);
+app.get('/users/:id', userController.buscarUsuario);
+app.post('/users', userController.adicionarUsuario);
+app.delete('/users/:id', userController.removerUsuario);
+app.put('/users/:id', userController.atualizarUsuario);
 
-// app.get('/books', bookController.listarLivros);
-// app.get('/books/:id', bookController.buscarLivro);
-// app.post('/books', bookController.adicionarLivro);
-// app.delete('/books/:id', bookController.removerLivro);
-// app.put('/books/:id', bookController.atualizarLivro);
-// app.put('/alugar/:id', bookController.alugarLivro);
-// app.put('/devolver/:id', bookController.devolverLivro);
-// app.get('/alugados', bookController.listarLivrosAlugados);
-// app.get('/disponiveis', bookController.listarLivrosDisponiveis);
+app.get('/books', bookController.listarLivros);
+app.get('/books/:id', bookController.buscarLivro);
+app.post('/books', bookController.adicionarLivro);
+app.delete('/books/:id', bookController.removerLivro);
+app.put('/books/:id', bookController.atualizarLivro);
+app.put('/alugar/:id', bookController.alugarLivro);
+app.put('/devolver/:id', bookController.devolverLivro);
+app.get('/alugados', bookController.listarLivrosAlugados);
+app.get('/disponiveis', bookController.listarLivrosDisponiveis);
 
 
 
